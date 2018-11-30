f5018df3d8981f88500a62c66a4887d057147cb5
jfrbks <41306593+jfrbks@users.noreply.github.com>
2018-11-30 08:59:34 -0600

    Renamed file

---
+++ houVPNProfile.ps1
@@ -0,0 +1,14 @@
+﻿$text=
+'<?xml version="1.0" encoding="UTF-8"?>
+<AnyConnectProfile xmlns="http://schemas.xmlsoap.org/encoding/">
+  <ServerList>
+    <HostEntry>
+      <HostName>Houston (SSL)</HostName>
+      <HostAddress>houvpn.tcoreinfinity.com:44443</HostAddress>
+      <PrimaryProtocol>SSL</PrimaryProtocol>
+    </HostEntry>
+  </ServerList>
+</AnyConnectProfile>
+'
+
+$text | Out-File -Encoding utf8 "C:\ProgramData\Cisco\Cisco AnyConnect Secure Mobility Client\Profile\houston_profile.xml"
