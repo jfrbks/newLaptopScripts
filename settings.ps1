5be0a98db5ca9d4eb8bc8c28bc748095dd965da2
jfrbks <41306593+jfrbks@users.noreply.github.com>
2018-12-06 08:47:55 -0600

    Added enable file ext and set time zone

--- settings.ps1
+++ settings.ps1
@@ -3,3 +3,13 @@
 (Get-WmiObject -Class "Win32_TSGeneralSetting" -Namespace root\cimv2\TerminalServices -Filter "TerminalName='RDP-tcp'").SetUserAuthenticationRequired(0) | Out-Null
 Get-NetFirewallRule -DisplayName "Remote Desktop*" | Set-NetFirewallRule -enabled true
 
+#Set Time Zone
+Set-TimeZone "Central Standard Time"
+
+#Turn On File Extensions
+$key = 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced'
+Set-ItemProperty $key Hidden 0
+Set-ItemProperty $key HideFileExt 0
+Set-ItemProperty $key ShowSuperHidden 0
+Stop-Process -processname explorer
+
